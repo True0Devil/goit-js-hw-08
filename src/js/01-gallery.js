87bbbb3cf2f64652e01609c555412173e77a4925
Daniil Plakhotnuk <dplakhotnuk@gmail.com>
2022-11-06 17:17:13 +0200

    completes task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+let markup = '';
+
+for (const image of galleryItems) {
+  markup += `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`;
+}
+
+galleryRef.innerHTML = markup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+galleryRef.addEventListener('click', onGalleryImageClick);
+
+function onGalleryImageClick(e) {
+  if (e.target.nodeName !== 'IMG') {
+    console.log('This is not a picture');
+    return;
+  }
+
+  e.preventDefault();
+
+  lightbox.open;
+}
