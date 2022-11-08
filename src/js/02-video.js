5a72457276c498a1aacac14e185bbea424dffcd7
Daniil Plakhotnuk <dplakhotnuk@gmail.com>
2022-11-08 14:04:35 +0200

    completes task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,16 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-player.on('timeupdate', onTimeUpdate);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
 
+function onTimeUpdate(data) {
+    localStorage.setItem(CURRENT_TIME_KEY, data.seconds);
+}
+
+if (localStorage.getItem(CURRENT_TIME_KEY)) {
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY));
+}
